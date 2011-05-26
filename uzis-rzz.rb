886206a6731b03c3d4b944eb85405c6dfa9b4bb7
Jan Stránský <jan.stransky@arnal.cz>
2011-05-26 03:34:51 +0200

    first commit

---
+++ uzis-rzz.rb
@@ -0,0 +1,147 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'openssl'
+require 'json'
+require 'cgi'
+require 'digest/md5'
+require 'progressbar'
+
+############ config
+
+base_uri = 'https://snzr.uzis.cz/viewzz/RZZHledat1.htm'
+base_region_uri = 'https://snzr.uzis.cz/viewzz/lb/RZZSeznam.pl?KRAJ={id}&ORP=V%8AE&OBEC=V%8AE&TYP=V%8AE&DRZAR=V%8AE&ZRIZOVATEL=V%8AE&NAZEV=V%8AE&Hledat=Hledat&WAIT_PAGE=ON'
+base_item_uri = 'https://snzr.uzis.cz/viewzz/lb/RZZDetail.pl?{id}=Detail&WAIT_PAGE=ON'
+
+DB_FILE = './data.json'
+CACHE_DIR = './cache'
+
+############ app
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+class String
+	def sanity
+		self.gsub(/\302\240/, " ").strip
+	end
+end
+def get_page(uri)
+	fname = CACHE_DIR + "/" + Digest::MD5.hexdigest(uri) + ".html"
+	if File.exists?(fname) && File.size(fname) > 0
+		file = File.open(fname).read
+	else
+		file = open(uri).read
+		File.open(fname, 'w') { |f| f.write(file) }
+	end
+	Nokogiri::HTML(file)
+end
+def msg(text)
+	$stdout.write text
+	$stdout.flush
+end
+
+def parse_item(uri)
+	# zakladni udaje
+	doc = get_page(uri)
+	data = {}
+
+	doc.css('table[width="616"][border="1"] tr').each do |r|
+		col = r.css('td:nth-child(1)').text.sanity
+		data[col] = r.css('td:nth-child(2)').text.sanity
+	end
+
+	doc.css('table[width="617"]').each do |t| 
+		name = t.css('tr:nth-child(1) td').first.text.sanity
+		case name
+			# oddeleni
+			when "Oddělení"
+				data[name] = []
+				cols = []
+				t.css('tr:nth-child(1) td').each { |cn| cols << cn.text.sanity }
+				t.css('tr:not(:nth-child(1))').each do |r|
+					part = {}; i = 0
+					r.css('td').each do |c|
+						cn = cols[i]
+						part[cn] = c.text.sanity
+						i = i+1
+					end
+					data[name] << part
+				end
+
+			# smlouvy s pojistovnami
+			when "smlouvy s pojišťovnami :"
+				data[name] = []
+				t.css('tr:not(:nth-child(1))').each do |r|
+					data[name] << r.text.sanity
+				end
+		end
+	end
+	data
+end
+
+def parse_list(uri)
+	doc = get_page(uri)
+	base_vals = { :type_id => nil, :type_name => nil, :orp => nil }
+	list = []
+
+	doc.css('body > table').each do |t|
+		case t['width']
+			# druh zarizeni
+			when "643"
+				base_vals[:type_id], base_vals[:type_name] = t.text.sanity.split("  ")
+			# ORP
+			when "811"
+				base_vals[:orp] = t.text.sanity.match(/^ORP:  (.+)$/)[1]
+			when "800"
+				t.css('tr > td > table > tr > td > table > tr').each do |l|
+					next if l.css('td:nth-child(1)').first.text == 'Název'
+					rzz_id = l.css('td:nth-child(4) a').first['onclick'].match(/RZZDetail\.pl\?(\d+)=Detail/)[1]
+					list << rzz_id
+				end
+		end
+	end
+	list
+end
+
+# load db
+db = (File.exists?(DB_FILE) ? JSON::load( File.open( DB_FILE )) : {});
+msg "Database file: #{DB_FILE} (#{db.size} items)\n"
+
+# fetch regions
+regions = []
+get_page(base_uri).css('select#kraj option').each do |o|
+	regions << { :id => o['value'], :name => o.text }
+end
+
+# process
+begin
+	regions.each do |region|
+		msg "===> #{region[:name]} .. "
+		list = parse_list(base_region_uri.sub('{id}', CGI::escape(region[:id])))
+
+		# find loaded items
+		to_load = []
+		list.each { |i| (to_load << i) if !db.include?(i) }
+		msg "#{list.size} items on web, #{list.size-to_load.size} in local database, #{to_load.size} to fetch\n"
+
+
+		# fetch not loaded items
+		pbar = ProgressBar.new(region[:name], to_load.size)
+		to_load.each do |i|
+			db[i] = parse_item(base_item_uri.sub('{id}', i))
+			db[i]['id'] = i
+			pbar.inc
+		end
+		pbar.finish
+	end
+
+rescue Exception
+	msg($!.class.to_s == "Interrupt" ? "\n\nPressing CTRL-C. Aborting .. \n" : "\n\nerror: [#{$!.class}] #{$!}\nbacktrace:\n#{$!.backtrace.join("\n")}\n\n")
+ensure
+	# save database
+	msg "Saving database .. "
+	File.open(DB_FILE, "w") { |f| f.write(JSON.dump(db)) }
+	msg "ok\n"
+end
+
